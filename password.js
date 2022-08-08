d8f3c52800c63f91459bb582e677a541a13cebc5
collinburnsoll <109363180+collinburnsoll@users.noreply.github.com>
2022-08-08 12:51:50 -0400

    Update password.js

--- password.js
+++ password.js
@@ -16,31 +16,21 @@ function enterPassword() {
 		alert('Wrong Password! Please try again')
 	}
 };
-	
-	/*function createCookie(name, value, days) {
-	    var expires;
-	    if (days) {
-	        var date = new Date();
-	        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-	        expires = "; expires=" + date.toGMTString();
-	    }
-	    else {
-	        expires = "";
-	    }
-	    document.cookie = name + "=" + value + expires + "; path=/";
-	}
-	
-	function getCookie(c_name) {
-	    if (document.cookie.length > 0) {
-	        c_start = document.cookie.indexOf(c_name + "=");
-	        if (c_start != -1) {
-	            c_start = c_start + c_name.length + 1;
-	            c_end = document.cookie.indexOf(";", c_start);
-	            if (c_end == -1) {
-	                c_end = document.cookie.length;
-	            }
-	            return unescape(document.cookie.substring(c_start, c_end));
-	        }
-	    }
-	    return "";
-	}*/
+
+function openHome() {
+		document.getElementById('home').style.display = 'block'; 
+		document.getElementById('games').style.display = 'none';
+		document.getElementById('settings').style.display = 'none';
+};
+
+function openGames() {
+		document.getElementById('home').style.display = 'none'; 
+		document.getElementById('games').style.display = 'block';
+		document.getElementById('settings').style.display = 'none';
+};
+
+function openSettings() {
+		document.getElementById('home').style.display = 'none'; 
+		document.getElementById('games').style.display = 'none';
+		document.getElementById('settings').style.display = 'settings';
+};
