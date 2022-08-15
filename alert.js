256be941e2998a0a629300301e6426881755fca0
collinburnsoll <109363180+collinburnsoll@users.noreply.github.com>
2022-08-15 13:23:53 -0400

    Update alert.js

--- alert.js
+++ alert.js
@@ -15,7 +15,7 @@ function cloakTab() {
 	    if (!popup || popup.closed) {
 	        alert("Allow popups and redirects to continue")
 	    } else {
-	        /*const doc = popup.document
+	        const doc = popup.document
 	        const iframe = doc.createElement("iframe")
 	        const style = iframe.style
 	        const link = doc.createElement("link")
@@ -30,12 +30,7 @@ function cloakTab() {
 	        style.width = style.height = "100%"
 	
 	        doc.body.appendChild(iframe)
-	        location.replace("https://google.com")*/
-  var tab = window.open('about:blank', '_blank');
-  tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>Classes</title><link rel="icon" type="image/png" href="https://ssl.gstatic.com/classroom/favicon.png"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="https://edu-links.pages.dev" frameborder="0"></iframe></body></html>';
-  tab.document.close();
-  
-  window.location.replace('https://www.google.com');
+	        location.replace("https://google.com")
 	    }
 	}
 	
