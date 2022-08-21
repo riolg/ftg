1ce2c9d70a9d71f894bd2b8b63fc7cc64053aad1
collinburnsoll <109363180+collinburnsoll@users.noreply.github.com>
2022-08-21 10:55:18 -0400

    added window.moveTo(0, 0)

--- password.js
+++ password.js
@@ -6,7 +6,8 @@ function enterPassword() {
 	if (password==pass1) {
 			alert('Wrong Password! Please try again')
 			document.getElementById('bottomLinks').style.display = 'block'; 
-			document.getElementById('topLinks').style.display = 'none'; 
+			document.getElementById('topLinks').style.display = 'none';
+			window.moveTo(0, 0);
 	}
 	else if (password==pass2) {
 			alert('Correct password! Click OK to continue')
